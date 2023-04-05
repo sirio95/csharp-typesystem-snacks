3110571721db20af198b606c3c8997d6c7f969d6
sirio95 <sirio.iasevoli@gmail.com>
2023-04-05 16:04:53 +0200

    consegna snack 4

--- csharp-typesystem-snacks/istruzioni-condizionali.cs
+++ csharp-typesystem-snacks/istruzioni-condizionali.cs
@@ -66,7 +66,7 @@ Console.WriteLine($"La somma degli ultimi 8 numeri inseriti è: {sumUserNumMin2}
 
 //Snack 3 - Operatore modulo
 
-Console.WriteLine("Inserisci un numero");
+/*Console.WriteLine("Inserisci un numero");
 
 int numEvenOdd;
 if (int.TryParse(Console.ReadLine(), out numEvenOdd))
@@ -78,7 +78,74 @@ if (int.TryParse(Console.ReadLine(), out numEvenOdd))
 }
 else
     Console.WriteLine("Il numero non è valido");
+*/
+
+//Snack 4- Array
+
+//festa fitzgerald
+
+ string[] partyname = { "nome1", "nome2", "nome3", "nome4", "nome5", "nome6", "nome7", "nome8", "nome9", "nome10" };
+
+Console.WriteLine("Inserisci il tuo nome per vedere se sei invitato a festa Gatsby");
+string? userName = Console.ReadLine();
+
+if (userName != null)
+{
+    bool invited = Array.Exists(partyname, element => element == userName);
+    if (invited)
+        Console.WriteLine($"{userName} sei nella lista. Goditi la festa!");
+    else
+        Console.WriteLine($"{userName} non sei invitato. Non puoi entrare!");
+}
+else
+    Console.WriteLine("Non hai inserito un nome valido");
 
 
+//numeri dispari (o forse pari?)
 
+var numEven = new List<int>();
+var numOdd = new List<int>();
+
+Console.WriteLine("Inserisci 6 numeri in successione");
+
+for(int i = 0; i < 6; i++)
+{
+    int numSnack4;
+    if (int.TryParse(Console.ReadLine(), out numSnack4))
+    {
+        if (numSnack4 % 2 == 0) 
+        numEven.Add(numSnack4); 
+        else
+            numOdd.Add(numSnack4);
+    }
+    else
+        Console.WriteLine("Il numero inserito non è valido");
+}
+
+int[] arrNumEven = numEven.ToArray();
+int[] arrNumOdd = numOdd.ToArray();
+
+Console.WriteLine($"I numeri dispari che hai scritto sono:");
+foreach (int num in arrNumOdd) Console.WriteLine(num); 
+
+
+Console.WriteLine($"Invece i numeri pari sono:");
+foreach (int num in arrNumEven) Console.WriteLine(num);
+
+//somma numeri ad indice dispari
+
+Console.WriteLine("Magia: vedrai prima un insieme di numeri e poi, magicamente, SOLO la somma di quelli in posizione dispari");
+int[] arrSumOdd = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+for (int i = 0; i < arrSumOdd.Length; i++) Console.WriteLine($"{i}: {arrSumOdd[i]}");
+var sumOdd = new List<int>();
+
+for (int i = 0; i < arrSumOdd.Length; i++)
+{
+    if (arrSumOdd[i] % 2 != 0) sumOdd.Add(arrSumOdd[i]);
+}
+int[] sumTotalOdd = sumOdd.ToArray();
+int sumTotal = sumTotalOdd.Sum();
+
+Console.WriteLine($"La somma totale degli elementi a indice dispari dell'array è: {sumTotal}");
 
