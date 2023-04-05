902627c9bea36ebf04a9e8484b5c2fbbb8cede63
sirio95 <sirio.iasevoli@gmail.com>
2023-04-05 14:19:47 +0200

    snack 1

---
+++ csharp-typesystem-snacks/istruzioni-condizionali.cs
@@ -0,0 +1,19 @@
+﻿//Snack 1 
+
+int num1;
+int num2;
+
+string parola1;
+string parola2;
+
+Console.WriteLine("Inserisci due numeri in successione");
+if (int.TryParse(Console.ReadLine(), out num1) && int.TryParse(Console.ReadLine(), out num2))
+{
+    if (num1 > num2)
+        Console.WriteLine($"il numero maggiore è: {num1}");
+    else if (num1 < num2)
+        Console.WriteLine($"il numero maggiore è: {num2}");
+    else Console.WriteLine($"i due numeri inseriti ({num1} e {num2}) sono uguali");
+}
+else
+    Console.WriteLine("Non hai inserito numeri validi");
